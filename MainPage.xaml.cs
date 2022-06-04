b8a9d9b4ce94310798642db7b582c366527ba1f5
BartoszSykutera <59446673+BartoszSykutera@users.noreply.github.com>
2022-06-04 20:55:44 +0200

    Dodanie rzeczy

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -20,31 +20,56 @@ namespace mediaplayer
     {
         private MediaPlayer mediaPlayer = new MediaPlayer();
         private readonly string saveFileName = ".\\mp3.json";
-        private ICollection<string> playerNames = new HashSet<string>();
+        private string playerNames = null;
+        //private List<string> playerNames = new List<string>();
+        private string playerPath = null;
+        private string imagePath = null;
+        private List<string> playerNamesIsoSto = new List<string>();
+        private List<string> playerPathIsoSto = new List<string>();
+        private List<string> playerImageIsoSto = new List<string>();
         private ViewModel viewModel = new ViewModel();
+        //List<MySong> songLists = new List<MySong>();
 
 
         public class ViewModel
         {
-            public ObservableCollection<string> audioTracks { get; set; } = new ObservableCollection<string>(); //{ new Obiekt { Nazwa = "a" }, new Obiekt { Nazwa = "b" } };
+            //public ObservableCollection<string> audioTracks { get; set; } = new ObservableCollection<string>(); //{ new Obiekt { Nazwa = "a" }, new Obiekt { Nazwa = "b" } };
+            public ObservableCollection<MySong> songLists { get; set; } = new ObservableCollection<MySong>(); //{ new Obiekt { Nazwa = "a" }, new Obiekt { Nazwa = "b" } };
+
 
         }
         public MainPage()
         {
             this.InitializeComponent();
             //dataGrid.ItemsSource = viewModel.audioTracks;
-            if (ApplicationData.Current.LocalSettings.Values.ContainsKey("listaPiosenek"))
+            if (ApplicationData.Current.LocalSettings.Values.ContainsKey("listaPiosenek")
+                && ApplicationData.Current.LocalSettings.Values.ContainsKey("listaSciezek"))
             {
+                List<string> nameses = JsonConvert.DeserializeObject<List<string>>((string)ApplicationData.Current.LocalSettings.Values["listaPiosenek"]);
+                List<string> pathes = JsonConvert.DeserializeObject<List<string>>((string)ApplicationData.Current.LocalSettings.Values["listaSciezek"]);
+                for (int i = 0; i < nameses.Count; i++)
+                {
+                    var song = nameses[i];
+                    var path = pathes[i];
+                    playerNamesIsoSto.Add(song);
+                    playerPathIsoSto.Add(path);
+                    viewModel.songLists.Add(new MySong(song, path, "Assets/white2115.jpg"));
+                }
+                /*
                 foreach (var item in JsonConvert.DeserializeObject<List<string>>((string)ApplicationData.Current.LocalSettings.Values["listaPiosenek"]))
                 {
-                    viewModel.audioTracks.Add(item);
+                    playerNamesIsoSto.Add(item);
+                    viewModel.songLists.Add(new MySong(item, item, "Assets/white2115.jpg"));
                 }
+                foreach (var item in JsonConvert.DeserializeObject<List<string>>((string)ApplicationData.Current.LocalSettings.Values["listaSciezek"]))
+                {
+                    playerPathIsoSto.Add(item);
+                    viewModel.songLists.Add(new MySong(item, item, "Assets/white2115.jpg"));
+                }
+                */
             }
-            listView.ItemsSource = viewModel.audioTracks;
-            viewModel.audioTracks.CollectionChanged += AudioTracks_CollectionChanged;
-
-
-
+            listView.ItemsSource = viewModel.songLists;
+            viewModel.songLists.CollectionChanged += AudioTracks_CollectionChanged;
         }
 
         private void AudioTracks_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
@@ -54,48 +79,82 @@ namespace mediaplayer
 
         private void RefreshDataGrid()
         {
-            viewModel.audioTracks.Clear();
-            foreach (string playerName in playerNames)
-            {
-                viewModel.audioTracks.Add(playerName);
-            }
+            //viewModel.songLists.Clear();
 
+            //foreach (string playerName in playerNames)
+            //{
+            //viewModel.audioTracks.Add(playerName);
+            //viewModel.songLists.Add(new MySong(playerName, playerName, "Assets/white2115.jpg"));
+            //}
+            if(playerNames!=null && playerPath != null)
+            {
+                playerNamesIsoSto.Add(playerNames);
+                playerPathIsoSto.Add(playerPath);
+                playerImageIsoSto.Add(imagePath);
+                viewModel.songLists.Add(new MySong(playerNames, playerPath, "Assets/white2115.jpg"));
+                playerNames = null;
+                playerPath = null;
+            }
+            
+            
+            //playerNames.ForEach(names => playerNamesIsoSto.Add(names));
+            //playerNames.Clear();
+            //playerPath.Clear();
+            
         }
         private async void Button_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            popup.IsOpen = true;
+        }
+        private async void importSong_Click(object sender, RoutedEventArgs e)
         {
             FileOpenPicker openPicker = new FileOpenPicker();
             openPicker.ViewMode = PickerViewMode.Thumbnail;
             openPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
             openPicker.FileTypeFilter.Add(".mp3");
 
+            StorageFile file = await openPicker.PickSingleFileAsync();
+            if (file != null)
+            {
+                mediaPlayer.SetFileSource(file);
+                playerPath = file.Path;
+                //playerPath.Add(file.Path);
+            }
+        }
+        private async void importImage_Click(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker openPicker = new FileOpenPicker();
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+            openPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+            openPicker.FileTypeFilter.Add(".png");
+            openPicker.FileTypeFilter.Add(".jpg");
 
             StorageFile file = await openPicker.PickSingleFileAsync();
             if (file != null)
             {
-
-                mediaPlayer.SetFileSource(file);
-                playerNames.Add(file.Path);
-                RefreshDataGrid();
-                mediaPlayer.Play();
-                DispatcherTimer timer = new DispatcherTimer();
-                timer.Interval = TimeSpan.FromSeconds(1);
-                timer.Tick += timer_Tick;
-                timer.Start();
+                imagePath = file.Path;
+                //playerPath.Add(file.Path);
             }
-
-
         }
-
-
-
+        private void closeImport_Click(object sender, RoutedEventArgs e)
+        {
+            playerNames = nazwaUtworu.Text;
+            RefreshDataGrid();
+            popup.IsOpen = false;
+            //mediaPlayer.Play();
+            //DispatcherTimer timer = new DispatcherTimer();
+            //timer.Interval = TimeSpan.FromSeconds(1);
+            //timer.Tick += timer_Tick;
+            //timer.Start();
+        }
         private void Save_songs()
         {
-            ApplicationData.Current.LocalSettings.Values["listaPiosenek"] = JsonConvert.SerializeObject(playerNames);
+            ApplicationData.Current.LocalSettings.Values["listaPiosenek"] = JsonConvert.SerializeObject(playerNamesIsoSto);
+            ApplicationData.Current.LocalSettings.Values["listaSciezek"] = JsonConvert.SerializeObject(playerPathIsoSto);
+            ApplicationData.Current.LocalSettings.Values["obrazy"] = JsonConvert.SerializeObject(playerNames);
             //Debug.WriteLine(JsonConvert.SerializeObject(playerNames));
 
         }
-
-
         void timer_Tick(object sender, object e)
         {
             if (mediaPlayer.GetAsCastingSource() != null)
@@ -120,7 +179,7 @@ namespace mediaplayer
             Pause.Visibility = Visibility.Collapsed;
             Play.Visibility = Visibility.Visible;
         }
-
+        /*
         private async void TextBlock_Tapped(object sender, TappedRoutedEventArgs e)
         {
             StorageFile file = await StorageFile.GetFileFromPathAsync(((TextBlock)sender).Text);
@@ -137,7 +196,24 @@ namespace mediaplayer
             }
             mediaPlayer.Play();
         }
+        */
+        private async void listView_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            var selectedSong = listView.SelectedItem as MySong;
 
+            StorageFile file = await StorageFile.GetFileFromPathAsync(selectedSong.PathName);
 
+            if (file != null)
+            {
+                string faToken = StorageApplicationPermissions.FutureAccessList.Add(file);
+                mediaPlayer.SetFileSource(await StorageApplicationPermissions.FutureAccessList.GetFileAsync(faToken));
+
+                DispatcherTimer timer = new DispatcherTimer();
+                timer.Interval = TimeSpan.FromSeconds(1);
+                timer.Tick += timer_Tick;
+                timer.Start();
+            }
+            mediaPlayer.Play();
+        }
     }
 }
---
+++ MySong.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mediaplayer
+{
+    public class MySong
+    {
+        public string Name { get; set; }
+        public string PathName { get; set; }
+        public string ImagePath { get; set; }
+
+        public MySong() { 
+        }
+
+        public MySong(string name, string pathName, string imagePath)
+        {
+            this.Name = name;
+            this.PathName = pathName;
+            this.ImagePath = imagePath;
+        }
+
+    }
+}
